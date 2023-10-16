4dec515f9215963c87b2efbfde6348edee1d9e75
joemust0 <joaofss.1991@gmail.com>
2023-10-16 20:11:28 -0300

    adição de rotas iniciais e formatação inicial para teste.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './componets/main/main.component';
+import { LoginComponent } from './componets/login/login.component';
+import { CadUsuarioComponent } from './componets/cad-usuario/cad-usuario.component';
+import { LancamentosComponent } from './componets/lancamentos/lancamentos.component';
+import { BalancoComponent } from './componets/balanco/balanco.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: MainComponent },
+  {path: 'login', component: LoginComponent },
+  {path: 'cadastro', component: CadUsuarioComponent },
+  {path: 'lançamentos', component: LancamentosComponent },
+  {path: 'balanço', component: BalancoComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
