29f2714364884cb2afa5d0acf6885a8ca84d8880
joemust0 <joaofss.1991@gmail.com>
2023-10-26 15:04:01 -0300

    função script do navbar implementado

--- src/app/componets/nav/nav.component.ts
+++ src/app/componets/nav/nav.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css']
 })
-export class NavComponent {
+export class NavComponent implements OnInit {
 
-  
+  constructor() { }
+
+  ngOnInit(): void {
+
+
+  const btn_menu = document.querySelector(".btn-menu") as HTMLElement;
+  const side_bar = document.querySelector(".sidebar") as HTMLElement;
+
+  btn_menu.addEventListener('click', () => {
+    side_bar.classList.toggle('expand');
+});
+}
 }
