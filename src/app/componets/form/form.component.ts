3f2096c2572187488821004afc1bd2d2c1bef97e
PauloDantasSilva <paulorobertordantass@gmail.com>
2023-10-30 22:33:02 -0300

    gerar mais formulários para  o balanço

--- src/app/componets/form/form.component.ts
+++ src/app/componets/form/form.component.ts
@@ -8,3 +8,13 @@ import { Component } from '@angular/core';
 export class FormComponent {
 
 }
+document.addEventListener('DOMContentLoaded', () => {
+    const adicionarLinhaButton = document.getElementById('Adicionar') as HTMLButtonElement;
+    adicionarLinhaButton.addEventListener('click', () => {
+        const tabelaBase = document.getElementById('Base') as HTMLTableElement;
+        const ultimaLinhaBase = tabelaBase.querySelector('tr:last-child') as HTMLTableRowElement;
+        const novaLinha = ultimaLinhaBase.cloneNode(true) as HTMLTableRowElement;
+        const tabelaNova = document.getElementById('Nova') as HTMLTableElement;
+        tabelaNova.appendChild(novaLinha);
+    });
+});
