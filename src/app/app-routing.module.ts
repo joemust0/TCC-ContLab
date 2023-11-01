facd049a3ede92d0110d3fc66d0432f7f0323e60
joemust0 <joaofss.1991@gmail.com>
2023-11-01 03:32:04 -0300

    Organização das pastas do projeto e correção do script lançamento.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { MainComponent } from './componets/main/main.component';
-import { LoginComponent } from './componets/login/login.component';
-import { CadUsuarioComponent } from './componets/cad-usuario/cad-usuario.component';
-import { LancamentosComponent } from './componets/lancamentos/lancamentos.component';
-import { BalancoComponent } from './componets/balanco/balanco.component';
-import { TestesComponent } from './componets/testes/testes.component';
+import { MainComponent } from './Front/main/main.component';
+import { LoginComponent } from './Front/Usuario/login/login.component';
+import { CadUsuarioComponent } from './Front/Usuario/cad-usuario/cad-usuario.component';
+import { LancamentosComponent } from './Front/ferramenta/lancamentos/lancamentos.component';
+import { BalancoComponent } from './Front/ferramenta/balanco/balanco.component';
+import { TestesComponent } from './Dev/testes/testes.component';
 
 
 const routes: Routes = [
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MainComponent } from './componets/main/main.component';
-import { HeaderComponent } from './componets/header/header.component';
-import { FooterComponent } from './componets/footer/footer.component';
-import { LoginComponent } from './componets/login/login.component';
-import { CadUsuarioComponent } from './componets/cad-usuario/cad-usuario.component';
-import { LancamentosComponent } from './componets/lancamentos/lancamentos.component';
-import { BalancoComponent } from './componets/balanco/balanco.component';
-import { NavComponent } from './componets/nav/nav.component';
-import { CadastroComponent } from './componets/tabelas/cadastro/cadastro.component';
-import { GraficoComponent } from './componets/tabelas/grafico/grafico.component';
-import { ScriptComponent } from './componets/php/script/script.component';
-import { TestesComponent } from './componets/testes/testes.component';
-import { MonologComponent } from './componets/php/monolog/monolog.component';
-import { SlimComponent } from './componets/php/slim/slim.component';
-import { MotorDeCalculoComponent } from './componets/motor-de-calculo/motor-de-calculo.component';
-import { FormComponent } from './componets/form/form.component';
+import { MainComponent } from './Front/main/main.component';
+import { HeaderComponent } from './Front/header/header.component';
+import { FooterComponent } from './Front/footer/footer.component';
+import { LoginComponent } from './Front/Usuario/login/login.component';
+import { CadUsuarioComponent } from './Front/Usuario/cad-usuario/cad-usuario.component';
+import { LancamentosComponent } from './Front/ferramenta/lancamentos/lancamentos.component';
+import { BalancoComponent } from './Front/ferramenta/balanco/balanco.component';
+import { NavComponent } from './Front/nav/nav.component';
+import { CadastroComponent } from './Front/ferramenta/cadastro/cadastro.component';
+import { GraficoComponent } from './Front/ferramenta/grafico/grafico.component';
+import { ScriptComponent } from './Dev/PHP/script/script.component';
+import { TestesComponent } from './Dev/testes/testes.component';
+import { MonologComponent } from './Dev/PHP/monolog/monolog.component';
+import { SlimComponent } from './Dev/PHP/slim/slim.component';
+import { MotorDeCalculoComponent } from './Dev/motor-de-calculo/motor-de-calculo.component';
+import { FormComponent } from './Front/ferramenta/form/form.component';
 
 
 
