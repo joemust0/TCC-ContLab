22b1764608007d8c70540348397ff8f7825fe2a8
joemust0 <joaofss.1991@gmail.com>
2023-11-06 16:04:36 -0300

    integração tela de criação de balanço com lançamentos

--- src/app/Front/ferramenta/balanco/balanco.component.ts
+++ src/app/Front/ferramenta/balanco/balanco.component.ts
@@ -1,3 +1,4 @@
+import { Router } from '@angular/router';
 import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
@@ -10,26 +11,23 @@ export class BalancoComponent {
   @ViewChild('btnNovo') oc!: ElementRef;
   @ViewChild('criarBalanco') ap!: ElementRef;
 
-constructor() {}
+  constructor(private router: Router) {}
 
   newBalanco() {
+    const nameInput = <HTMLInputElement>document.getElementById("name");
+    const descInput = <HTMLTextAreaElement>document.getElementById("description");
 
-  const nameInput = <HTMLInputElement>document.getElementById("name");
-  const descInput = <HTMLTextAreaElement>document.getElementById("description");
+    const name = nameInput.value;
+    const description = descInput.value;
 
-  const name = nameInput.value;
-  const description = descInput.value;
-
-  document.writeln("Nome: " + name);
-  document.writeln("Descrição: " + description);
-    }
-
-    nBalanco(){
-   this.oc.nativeElement.classList.add('ocultar')
-   this.ap.nativeElement.classList.remove('ocultar')
-
-    }
-
+    // Passando os valores como parâmetros na navegação
+    this.router.navigate(['/lançamentos'], {
+      queryParams: { nome: name, descricao: description }
+    });
   }
 
-
+  nBalanco() {
+    this.oc.nativeElement.classList.add('ocultar');
+    this.ap.nativeElement.classList.remove('ocultar');
+  }
+}
--- src/app/Front/ferramenta/lancamentos/lancamentos.component.ts
+++ src/app/Front/ferramenta/lancamentos/lancamentos.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-lancamentos',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./lancamentos.component.css']
 })
 export class LancamentosComponent {
+  
+  nome: string = "";
+  descricao: string = "";
 
+  constructor(private route: ActivatedRoute) {}
+
+ngOnInit() {
+  this.route.queryParams.subscribe(params => {
+    this.nome = params['nome'] ||  "";
+    this.descricao = params['descricao'] || "";
+
+  });
 }
+}
+
