cd1e87010a197118c708f25ca560764450b624c0
joemust0 <joaofss.1991@gmail.com>
2023-11-08 04:19:22 -0300

    funcionando comparativo

--- src/app/Dev/comparador/comparador.component.ts
+++ src/app/Dev/comparador/comparador.component.ts
@@ -1,5 +1,4 @@
-import { Component } from '@angular/core';
-import { ComparadorService } from './comparador.service';
+import { Component, ElementRef, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-comparador',
@@ -7,20 +6,26 @@ import { ComparadorService } from './comparador.service';
 })
 export class ComparadorComponent {
   contaO: number = 0;
-  contaD: number = 0;
-  temDiferenca: boolean = false;
+  contaD: number= 0;
+  diferenca: number = 0;
+  campoFaltando: string = ''; // Adicione uma propriedade para o campo que falta valor
 
-  constructor(private comparadorService: ComparadorService) {}
+  constructor() {}
 
   compararValores() {
-    this.comparadorService.compararValores(this.contaO, this.contaD).subscribe(result => {
-      this.temDiferenca = result;
-      if(result){
-        this.contaD++;
-      }
-      else{
-        this.contaO++;
-      }
-    });
+    if (this.contaO === 0 && this.contaD === 0) {
+      // Ambos os campos estão vazios
+      this.campoFaltando = 'Ambos os campos';
+    } else if (this.contaO === 0) {
+      // Campo 1 está vazio
+      this.campoFaltando = 'Campo 1';
+    } else if (this.contaD === 0) {
+      // Campo 2 está vazio
+      this.campoFaltando = 'Campo 2';
+    } else {
+      // Ambos os campos estão preenchidos, calcula a diferença
+      this.campoFaltando = '';
+      this.diferenca = this.contaO - this.contaD;
+    }
   }
 }
