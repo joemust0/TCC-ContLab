5a2fc9f158c42ce4779acd71dc0870ce21c4c1ac
brauds <henrytmomaz@gmail>
2024-04-26 22:17:27 -0300

    realizei outras mudanças na tela de cadastro

--- src/app/Front/main/main.component.ts
+++ src/app/Front/main/main.component.ts
@@ -8,3 +8,20 @@ import { Component } from '@angular/core';
 export class MainComponent {
 
 }
+// script.ts
+function updateInfo() {
+  const infoElement = document.getElementById("info");
+  if (infoElement) {
+    const currentDate = new Date();
+    const formattedDate = `${currentDate.getDate()}/${currentDate.getMonth() + 1}/${currentDate.getFullYear()}`;
+    infoElement.textContent = `Última atualização em ${formattedDate}`;
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  const updateButton = document.getElementById("updateButton");
+  if (updateButton) {
+    updateButton.addEventListener("click", updateInfo);
+  }
+});
+
