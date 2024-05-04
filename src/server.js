f3fef039879d57c69765488c16ca9b0f8a6a5a2e
joemut0 <joaofss.1991@gmail.com>
2024-05-04 01:15:28 -0300

    Start API ContLab

---
+++ src/server.js
@@ -0,0 +1,18 @@
+require('dotenv').config({ path: '../variaveis.env' });
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const routes = require('./routes');
+
+const server = express();
+server.use(cors());
+server.use(bodyParser.urlencoded({extended: false}));
+
+// Definir as rotas
+server.use('/', routes);
+
+// Iniciar o servidor
+server.listen(process.env.PORT, () => {
+    console.log(`Servidor rodando em: http://localhost:${process.env.PORT}`);
+});
